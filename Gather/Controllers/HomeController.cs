34dca648e53e49a84459b56457b6e96c56d2aa3c
workmanmcr <workmanmcr@gmail.com>
2023-08-21 16:17:14 -0700

    merge all current content

---
+++ Gather/Controllers/HomeController.cs
@@ -0,0 +1,18 @@
+﻿using System.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
+using Gather.Models;
+
+namespace Gather.Controllers;
+
+public class HomeController : Controller
+{
+
+
+   
+
+    public IActionResult Index()
+    {
+        return View();
+    }
+
+    }
