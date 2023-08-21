3b46be5875bc6cf31d8c9a7dd5f66554e47196d6
workmanmcr <workmanmcr@gmail.com>
2023-08-21 16:10:50 -0700

    merger all

--- Gather/Controllers/HomeController.cs
+++ Gather/Controllers/HomeController.cs
@@ -6,16 +6,13 @@ namespace Gather.Controllers;
 
 public class HomeController : Controller
 {
-    private readonly ILogger<HomeController> _logger;
 
-    public HomeController(ILogger<HomeController> logger)
-    {
-        _logger = logger;
-    }
+
+   
 
     public IActionResult Index()
     {
         return View();
     }
 
-}
+    }
