176cb487c05a6553b8b98e372be0f7eae850f94e
workmanmcr <workmanmcr@gmail.com>
2023-08-21 15:26:38 -0700

    create Item model

---
+++ Gather/Models/Item.cs
@@ -0,0 +1,12 @@
+using System;
+using System.Collections.Generic;
+
+namespace Gather.Models
+{
+  public class Item
+  {
+    public int ItemId { get; set; }
+    public string ItemName { get; set; }
+  
+  }
+}
