10781dcfe24fe9ca91fa4f907deb0008170dd205
Joshua Khan <Khanjo@gmail.com>
2023-08-23 12:36:55 -0700

    fixing activies index view

---
+++ Gather/Migrations/20230823182333_NewModelProperties2.cs
@@ -0,0 +1,97 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Gather.Migrations
+{
+    public partial class NewModelProperties2 : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_UserActivities_AspNetUsers_UserId1",
+                table: "UserActivities");
+
+            migrationBuilder.DropIndex(
+                name: "IX_UserActivities_UserId1",
+                table: "UserActivities");
+
+            migrationBuilder.DropColumn(
+                name: "UserId1",
+                table: "UserActivities");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "UserId",
+                table: "UserActivities",
+                type: "varchar(255)",
+                nullable: true,
+                oldClrType: typeof(int),
+                oldType: "int")
+                .Annotation("MySql:CharSet", "utf8mb4");
+
+            migrationBuilder.AddColumn<int>(
+                name: "ApplicationUserId",
+                table: "UserActivities",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_UserActivities_UserId",
+                table: "UserActivities",
+                column: "UserId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_UserActivities_AspNetUsers_UserId",
+                table: "UserActivities",
+                column: "UserId",
+                principalTable: "AspNetUsers",
+                principalColumn: "Id");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_UserActivities_AspNetUsers_UserId",
+                table: "UserActivities");
+
+            migrationBuilder.DropIndex(
+                name: "IX_UserActivities_UserId",
+                table: "UserActivities");
+
+            migrationBuilder.DropColumn(
+                name: "ApplicationUserId",
+                table: "UserActivities");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "UserId",
+                table: "UserActivities",
+                type: "int",
+                nullable: false,
+                defaultValue: 0,
+                oldClrType: typeof(string),
+                oldType: "varchar(255)",
+                oldNullable: true)
+                .OldAnnotation("MySql:CharSet", "utf8mb4");
+
+            migrationBuilder.AddColumn<string>(
+                name: "UserId1",
+                table: "UserActivities",
+                type: "varchar(255)",
+                nullable: true)
+                .Annotation("MySql:CharSet", "utf8mb4");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_UserActivities_UserId1",
+                table: "UserActivities",
+                column: "UserId1");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_UserActivities_AspNetUsers_UserId1",
+                table: "UserActivities",
+                column: "UserId1",
+                principalTable: "AspNetUsers",
+                principalColumn: "Id");
+        }
+    }
+}
