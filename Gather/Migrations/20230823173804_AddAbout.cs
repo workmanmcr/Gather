6d48903192f67b5d3ff88d1869192631e4fc81dc
workmanmcr <workmanmcr@gmail.com>
2023-08-23 10:39:55 -0700

    fix Gathering bug

---
+++ Gather/Migrations/20230823173804_AddAbout.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Gather.Migrations
+{
+    public partial class AddAbout : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<DateTime>(
+                name: "DateTime",
+                table: "Gatherings",
+                type: "datetime(6)",
+                nullable: false,
+                oldClrType: typeof(int),
+                oldType: "int");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<int>(
+                name: "DateTime",
+                table: "Gatherings",
+                type: "int",
+                nullable: false,
+                oldClrType: typeof(DateTime),
+                oldType: "datetime(6)");
+        }
+    }
+}
